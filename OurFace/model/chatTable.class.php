973c0148bf1a40d9b0018b8a19a9171cc4624350
Briot-Ribeyre Baptiste <uapv1604137@adm-2w22a-alex.univ-avignon.fr>
2016-11-08 14:53:45 +0100

    ajout de l'entité chat

---
+++ OurFace/model/chatTable.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Classe Outils en lien avec l'entité chat
+	composée de méthodes statiques
+*/
+
+class chatTable {
+
+	public static function getChats() {
+
+	}
+
+	public static function getLastChats() {
+		
+	}
+}
+
+?>
