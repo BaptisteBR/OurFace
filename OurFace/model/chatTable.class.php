2f26d2189209786a70fea915cecefbe0c5ee1763
Briot-Ribeyre Baptiste <uapv1604137@adm-2w22a-alex.univ-avignon.fr>
2016-11-08 15:02:09 +0100

    Méthodes statiques de l'entité chat

--- OurFace/model/chatTable.class.php
+++ OurFace/model/chatTable.class.php
@@ -7,11 +7,25 @@
 class chatTable {
 
 	public static function getChats() {
-
+		$em = dbconnection::getInstance()->getEntityManager();
+		$chatRepository = $em->getRepository('chat');
+		$chats = $chatRepository->findAll();	
+	
+		if ($chats == false) {
+			echo 'Erreur sql';
+		}
+		return $chats;
 	}
 
-	public static function getLastChats() {
-		
+	public static function getLastChat() {
+		$em = dbconnection::getInstance()->getEntityManager();
+		$chatRepository = $em->getRepository('chat');
+		//$chat = $chatRepository->findOneBy();
+
+		if($chat == false) {
+			echo 'Erreur sql';
+		}
+		return $chat;
 	}
 }
 
